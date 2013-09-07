05ad24437e6f06cdddbd5cb14b8c1b30ad29e5e7
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2013-09-07 11:37:46 +0900

    add comments. fix a river problem

--- src/main/java/org/codelibs/elasticsearch/sample/module/SampleRiverModule.java
+++ src/main/java/org/codelibs/elasticsearch/sample/module/SampleRiverModule.java
@@ -2,10 +2,11 @@ package org.codelibs.elasticsearch.sample.module;
 
 import org.codelibs.elasticsearch.sample.river.SampleRiver;
 import org.elasticsearch.common.inject.AbstractModule;
+import org.elasticsearch.river.River;
 
 public class SampleRiverModule extends AbstractModule {
     @Override
     protected void configure() {
-        this.bind(SampleRiver.class).asEagerSingleton();
+        bind(River.class).to(SampleRiver.class).asEagerSingleton();
     }
 }
