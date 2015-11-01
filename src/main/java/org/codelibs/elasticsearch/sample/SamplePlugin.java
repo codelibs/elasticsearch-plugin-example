7401d2795bc134098e6fc37ac5ad4e857f45220e
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-11-01 22:16:16 +0900

    es 2.0 support

--- src/main/java/org/codelibs/elasticsearch/sample/SamplePlugin.java
+++ src/main/java/org/codelibs/elasticsearch/sample/SamplePlugin.java
@@ -5,13 +5,14 @@ import java.util.Collection;
 import org.codelibs.elasticsearch.sample.module.SampleModule;
 import org.codelibs.elasticsearch.sample.rest.SampleRestAction;
 import org.codelibs.elasticsearch.sample.service.SampleService;
-import org.elasticsearch.common.collect.Lists;
 import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
-import org.elasticsearch.plugins.AbstractPlugin;
+import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestModule;
 
-public class SamplePlugin extends AbstractPlugin {
+import com.google.common.collect.Lists;
+
+public class SamplePlugin extends Plugin {
     @Override
     public String name() {
         return "SamplePlugin";
@@ -22,26 +23,20 @@ public class SamplePlugin extends AbstractPlugin {
         return "This is a sample plugin.";
     }
 
-    // for Rest API
     public void onModule(final RestModule module) {
         module.addRestAction(SampleRestAction.class);
     }
 
-    // for Service
     @Override
-    public Collection<Class<? extends Module>> modules() {
-        final Collection<Class<? extends Module>> modules = Lists
-                .newArrayList();
-        modules.add(SampleModule.class);
+    public Collection<Module> nodeModules() {
+        final Collection<Module> modules = Lists.newArrayList();
+        modules.add(new SampleModule());
         return modules;
     }
 
-    // for Service
-    @SuppressWarnings("rawtypes")
     @Override
-    public Collection<Class<? extends LifecycleComponent>> services() {
-        final Collection<Class<? extends LifecycleComponent>> services = Lists
-                .newArrayList();
+    public Collection<Class<? extends LifecycleComponent>> nodeServices() {
+        final Collection<Class<? extends LifecycleComponent>> services = Lists.newArrayList();
         services.add(SampleService.class);
         return services;
     }
